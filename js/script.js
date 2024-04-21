722874bc8a11184d475636a1e5fa7a4f9bebab18
erditona <erditonaushaadam@gmail.com>
2024-04-21 23:47:24 +0700

    Refactor temperature conversion logic and update UI in index.html and script.js

--- js/script.js
+++ js/script.js
@@ -1,40 +1,39 @@
 "use strict";
 
-document.getElementById("button").addEventListener("click", toggleError);
+document.getElementById("buttonKonversi").addEventListener("click", convertToCelsius);
+document.getElementById("buttonKonversiReverse").addEventListener("click", convertToFahrenheit);
+document.querySelector("button[type='reset']").addEventListener("click", resetForm);
 
-function toggleError() {
-  // Remove previous result and error message
-  const resultElement = document.getElementById("result");
-  resultElement.textContent = "";
-
-  const errorMessage = document.getElementById("error");
-  errorMessage.classList.add("hidden");
-
-  // Convert Celsius to Fahrenheit
+function convertToCelsius() {
   const celsiusInput = document.querySelector("input[name='temperature']");
   const celsius = parseFloat(celsiusInput.value);
 
-  // Check if input is a valid number
   if (isNaN(celsius)) {
-    // Show error message
-    errorMessage.classList.remove("hidden");
-
-    // Highlight input and label with red
-    const allBorders = document.querySelectorAll(".border-gray-200");
-    const allTexts = document.querySelectorAll(".text-gray-500");
-    allBorders.forEach((el) => {
-      el.classList.add("border-red-600");
-    });
-    allTexts.forEach((el) => {
-      el.classList.add("text-red-600");
-    });
-
-    return; // Exit function if there's an error
+    document.getElementById("error").classList.remove("hidden");
+    return;
   }
 
-  // Calculate Fahrenheit
   const fahrenheit = (celsius * 9) / 5 + 32;
+  document.getElementById("result").textContent = `${celsius} Celsius is equal to ${fahrenheit.toFixed(2)} Fahrenheit`;
+  document.getElementById("error").classList.add("hidden");
+}
 
-  // Display result
-  resultElement.textContent = `${celsius} Celsius sama dengan ${fahrenheit.toFixed(2)} Fahrenheit`;
+function convertToFahrenheit() {
+  const celsiusInput = document.querySelector("input[name='temperature']");
+  const fahrenheit = parseFloat(celsiusInput.value);
+
+  if (isNaN(fahrenheit)) {
+    document.getElementById("error").classList.remove("hidden");
+    return;
+  }
+
+  const celsius = ((fahrenheit - 32) * 5) / 9;
+  document.getElementById("result").textContent = `${fahrenheit} Fahrenheit is equal to ${celsius.toFixed(2)} Celsius`;
+  document.getElementById("error").classList.add("hidden");
+}
+
+function resetForm() {
+  document.querySelector("input[name='temperature']").value = "";
+  document.getElementById("result").textContent = "";
+  document.getElementById("error").classList.add("hidden");
 }
